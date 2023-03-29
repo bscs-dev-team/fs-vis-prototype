7ff3a4291c9d4a3862dce812ef3a1269fef80c3f
bscs-dev-team <devops@bscs.org>
2023-03-29 11:09:54 -0700

    Move tableData to Explore.js

--- src/Explore.js
+++ src/Explore.js
@@ -1,4 +1,4 @@
-import React, { useCallback } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { useImmer } from "use-immer";
 
 import './Explore.css';
@@ -8,7 +8,68 @@ import FilterSet from './FilterSet';
 import MapTable from './MapTable';
 import GraphArea from './GraphArea';
 
+const csv2json = require('csvtojson');
+import csvFileUrl from 'url:./data/frogs.csv';
+
+// CSV DATA LOADING
+let alreadyLoaded = false;
+const fieldsToShow = [
+  "Observation Date",
+  "Start Time",
+  "End Time",
+  "Species",
+  "Call Intensity",
+  "Air Temperature",
+  "Characterize Land Use",
+  "Observation Notes"
+]
+
+
 export default function Explore() {
+  // CSV
+  const [tableData, setTableData] = useState({});
+  const [data, setData] = useState([]);
+  const [headers, setHeaders] = useState([]);
+  // -- Loader Function
+  async function loadCSV() {
+    if (alreadyLoaded) return console.log('...skipping');
+    console.log('...loadCSV!')
+    await fetch(csvFileUrl)
+      .then(r => r.text())
+      .then(text => {
+        console.log('......result', alreadyLoaded)
+        alreadyLoaded = true;
+        csv2json()
+          .fromString(text)
+          .then((json) => {
+            if (json === undefined) return console.error('Could not load CSV!');
+            // setHeaders( Object.keys(json[0]).filter( h => fieldsToShow.includes(h) ) );
+            setHeaders( fieldsToShow );
+            setData( () => {
+              const fieldData = json.map(row => {
+                const items = [];
+                fieldsToShow.forEach(k => items.push(row[k]));
+                return items;
+              });
+              console.log('fieldData', fieldData)
+              return fieldData;
+            });
+          });
+      });
+  }
+  // -- Init Load
+  useEffect(() => {
+    console.error('useEffect!', alreadyLoaded)
+    loadCSV();
+    setTableData({
+      count: data.length,
+      data: data,
+      headers: headers
+    })
+    return
+  }, [data, headers]);
+
+
   // Exploration Data
   const [ e, setE ] = useImmer({
     selectedFilterSetIndex: 0,
@@ -186,7 +247,7 @@ export default function Explore() {
         </div>
       </div>
       <div style={{ flexGrow: 1, overflow: 'hidden' }}>
-        <MapTable data={e} />
+        <MapTable tableData={tableData} />
       </div>
       <GraphArea 
         graphData={e.filtersets[e.selectedFilterSetIndex].graphs[e.selectedGraphIndex]}
--- src/MapTable.js
+++ src/MapTable.js
@@ -2,16 +2,16 @@ import './Exploration.css';
 import DataTable from './DataTable';
 import map from './img/vis-widget-card-map.png';
 
-export default function MapTable({data, handleAddFilter}) {
+export default function MapTable({tableData, handleAddFilter}) {
 
   return (
     <div className="MapTable" style={{ display: 'flex', flexDirection: 'column'}}>
         <div style={{ display: "flex" }}>
-          <div className="stats" style={{ flexGrow: 1 }}>{data.count} Observations</div>
+          <div className="stats" style={{ flexGrow: 1 }}>{tableData.count} Observations</div>
           <button>Table/Map</button>
         </div>
         <div style={{ display: "grid", gridTemplateColumns: "80% 20%", gridTemplateRows: "100%", height: "100%", overflow: "hidden"}}>
-            <DataTable data={data} />
+            <DataTable tableData={tableData} />
             <img src={map} alt="map"/>
         </div>
     </div>
