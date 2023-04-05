801074560ab0c0bb4ce3902e294b06b9a6d874e0
bscs-dev-team <devops@bscs.org>
2023-04-05 13:04:53 -0700

    Reverse history

--- src/DataTable.js
+++ src/DataTable.js
@@ -1,6 +1,16 @@
 import './Exploration.css';
 
 export default function DataTable({tableData}) {
+  const HACK = [
+    "Observation Date is the date the data was observed and recorded",
+    "Start Time is the local time when the participant started to listen/record for frog sounds",
+    "End Time is the local time when the participant stopped listening/recording frog sounds",
+    "Species is the frog species heard",
+    "Call Intensity is the number of individuals heard",
+    "Air Temperature in Celcius",
+    "Characterize Land Use is the type of land area",
+    "Observation Notes are additional notes taken during the observation"
+  ]
   console.log('table render')
   return (
     <div className="DataTable" style={{ display: "grid" }}>
@@ -8,7 +18,7 @@ export default function DataTable({tableData}) {
           <thead>
             <tr>
               {tableData.headers.map((h, i) => (
-                  <th key={i}>{h}</th>
+                  <th key={i}><div title={HACK[i]}>{h}</div></th>
               ))}
             </tr>
           </thead>
@@ -17,7 +27,7 @@ export default function DataTable({tableData}) {
               <tr key={i}>
                 {row.map((item, i) => (
                   <td key={i}>
-                    <div style={{ height: '1.2em', overflow: 'hidden'}}>{item}</div>
+                    <div style={{ height: '1.2em', overflow: 'hidden'}} title={item}>{item}</div>
                   </td>
                 ))}
               </tr>
